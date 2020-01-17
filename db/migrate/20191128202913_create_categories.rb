6d18393dc31e5886be7b9796932198f53f86c763
Damian Podlejski <damian.podlejski@airhelp.com>
2020-01-17 07:20:26 +0100

    Correct typo

--- db/migrate/20191128202913_create_categories.rb
+++ db/migrate/20191128202913_create_categories.rb
@@ -1,8 +1,8 @@
 class CreateCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :categories do |t|
-      t.string name
-      t.text description
+      t.string :name
+      t.text :description
 
       t.timestamps
     end
---
+++ db/schema.rb
@@ -0,0 +1,118 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20191128210518) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "company_name"
+    t.string "contact_name"
+    t.string "contact_title"
+    t.string "address"
+    t.string "city"
+    t.string "region"
+    t.string "postal_code"
+    t.string "country"
+    t.string "phone"
+    t.string "fax"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "last_name"
+    t.string "first_name"
+    t.string "title"
+    t.string "title_of_courtesy"
+    t.date "birthdate"
+    t.date "hiredate"
+    t.string "address"
+    t.string "city"
+    t.string "region"
+    t.string "postal_code"
+    t.string "country"
+    t.string "homephone"
+    t.string "extension"
+    t.text "notes"
+    t.string "reports_to"
+    t.string "photo_path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.decimal "unit_price"
+    t.integer "quantity"
+    t.decimal "discount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.date "order_date"
+    t.date "required_date"
+    t.date "shipped_date"
+    t.decimal "freight"
+    t.string "ship_name"
+    t.string "ship_address"
+    t.string "ship_city"
+    t.string "ship_region"
+    t.string "ship_postalcode"
+    t.string "ship_country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "product_name"
+    t.integer "quantity_per_unit"
+    t.decimal "unit_price"
+    t.integer "units_in_stock"
+    t.integer "units_on_order"
+    t.integer "reorder_level"
+    t.boolean "discontinued"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shippers", force: :cascade do |t|
+    t.string "company_name"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "company_name"
+    t.string "contact_name"
+    t.string "address"
+    t.string "city"
+    t.string "region"
+    t.string "postal_code"
+    t.string "country"
+    t.string "phone"
+    t.string "fax"
+    t.string "homepage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
